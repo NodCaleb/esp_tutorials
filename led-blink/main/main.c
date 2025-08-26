ddcb6c8b13e06635cf0e114e9914f26ca9d50034
NodCaleb <nod.caleb@gmail.com>
2025-08-26 15:15:54 +0300

    GPIO and LED samples

---
+++ led-blink/main/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+#define LED_PIN 13
+
+void app_main(void)
+{
+  gpio_reset_pin(LED_PIN);
+  gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+  
+  int ON = 0;
+
+    while(true)
+    {
+        ON = !ON;
+        gpio_set_level(LED_PIN, ON);
+        vTaskDelay(1000/ portTICK_PERIOD_MS);
+    }
+}
